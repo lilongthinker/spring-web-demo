5d80a2cebb3a66bbe63ac8022911a20b6eb3380f
dingyue <dingyue@taobaao.com>
2021-09-17 19:57:11 +0800

    enlarge leak size

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -78,27 +78,25 @@ public class DemoApplication {
 		return ip;
 	}
 
-	private String getRemoteIp(HttpServletRequest request){
-		public static String getRemoteIp(HttpServletRequest request) {
-			String ip = request.getHeader("x-forwarded-for");
-			if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-				ip = request.getHeader("Proxy-Client-IP");
-			}
-			if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-				ip = request.getHeader("WL-Proxy-Client-IP");
-			}
-			if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-				ip = request.getRemoteAddr();
-			}
-			final String[] arr = ip.split(",");
-			for (final String str : arr) {
-				if (!"unknown".equalsIgnoreCase(str)) {
-					ip = str;
-					break;
-				}
-			}
-			return ip;
+	public static String getRemoteIp(HttpServletRequest request) {
+		String ip = request.getHeader("x-forwarded-for");
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getHeader("Proxy-Client-IP");
 		}
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getHeader("WL-Proxy-Client-IP");
+		}
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getRemoteAddr();
+		}
+		final String[] arr = ip.split(",");
+		for (final String str : arr) {
+			if (!"unknown".equalsIgnoreCase(str)) {
+				ip = str;
+				break;
+			}
+		}
+		return ip;
 	}
 
 	@GetMapping(value = "/ip")
